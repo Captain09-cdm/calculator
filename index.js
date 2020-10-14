f2aab145dcbf4b4409da72dfcb924928ad7fe491
Captain09-cdm <64560529+Captain09-cdm@users.noreply.github.com>
2020-10-14 18:35:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+function click1() {
+    document.calculator.input.value += 1;
+}
+
+function click2() {
+    document.calculator.input.value += 2;
+}
+
+function click3() {
+    document.calculator.input.value += 3;
+}
+
+function clickn() {
+    document.calculator.input.value += "+";
+    
+}
+
+function clicka() {
+    var Display = eval(document.calculator.input.value);
+    document.calculator.input.value=Display;
+}
+
+function click4() {
+    document.calculator.input.value += 4;
+}
+
+function click5() {
+    document.calculator.input.value += 5;
+}
+
+function click6() {
+    document.calculator.input.value += 6;
+}
+
+function click7() {
+    document.calculator.input.value += 7;
+}
+
+function click8() {
+    document.calculator.input.value += 8;
+}
+
+function click9() {
+    document.calculator.input.value += 9;
+}
+
+function clickf() {
+    document.calculator.input.value += "-";
+}
+
+function clickp() {
+    document.calculator.input.value += "/";
+}
+
+function clickx() {
+    document.calculator.input.value += "*";
+}
+
+function click8() {
+    document.calculator.input.value += 8;
+}
+function clickC() {
+    document.calculator.input.value ="";
+}
+function clickjk() {
+    document.calculator.input.value +="%";
+}
+function click0() {
+    document.calculator.input.value +="0";
+}
+function clicky() {
+    document.calculator.input.value +=".";
+}
